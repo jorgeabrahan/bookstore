9fcba175c3caaad548260ce85ec512f3f2056ec8
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-23 16:36:59 -0600

    Create form component to add a new book

---
+++ src/components/AddBook.jsx
@@ -0,0 +1,12 @@
+export default function AddBook() {
+  return (
+    <>
+      <h2>ADD NEW BOOK</h2>
+      <form>
+        <input placeholder="Book title" />
+        <input placeholder="Book author" />
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </>
+  );
+}
