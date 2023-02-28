71bd6dc5b968609e8768b9e904e80075790abef0
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-27 19:39:29 -0600

    Dispatch the checkStatus reducer and show message

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,3 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
+
 export default function Categories() {
-  return <button type="button">CHECK STATUS</button>;
+  const dispatch = useDispatch();
+  const { categories } = useSelector((store) => store.categories);
+
+  return (
+    <>
+      {typeof categories === 'string' && <p>{categories}</p>}
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(checkStatus());
+        }}
+      >
+        CHECK STATUS
+      </button>
+    </>
+  );
 }
