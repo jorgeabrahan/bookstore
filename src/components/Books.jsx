dad0bc067a30f620ec3df463bab07be6a1836429
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-27 19:27:46 -0600

    Send ID as prop to the Book component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -8,6 +8,7 @@ export default function Books() {
   const booksList = books.map((book) => (
     <Book
       key={book.id}
+      id={book.id}
       title={book.title}
       author={book.author}
     />
