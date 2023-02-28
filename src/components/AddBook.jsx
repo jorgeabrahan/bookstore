02802ce986961d73e565da8def51b3d81142f9b6
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-27 19:16:05 -0600

    Handle input changes and dispatch event to add a book

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,11 +1,47 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/booksSlice';
+
 export default function AddBook() {
+  const dispatch = useDispatch();
+  const [book, setBook] = useState({
+    title: '',
+    author: '',
+  });
+
+  const onChange = (element) => {
+    const { name, value } = element;
+    setBook((prev) => ({ ...prev, [name]: value }));
+  };
   return (
     <>
       <h2>ADD NEW BOOK</h2>
-      <form>
-        <input placeholder="Book title" />
-        <input placeholder="Book author" />
-        <button type="submit">ADD BOOK</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(addBook({
+            id: uuidv4(),
+            ...book,
+          }));
+        }}
+      >
+        <input
+          name="title"
+          placeholder="Book title"
+          value={book.title}
+          onChange={({ target }) => onChange(target)}
+        />
+        <input
+          name="author"
+          placeholder="Book author"
+          value={book.author}
+          onChange={({ target }) => onChange(target)}
+        />
+        <button type="submit">
+          ADD BOOK
+        </button>
       </form>
     </>
   );
