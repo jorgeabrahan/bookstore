b63e972506b0cd8831c130d6144c53ebd0735a70
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-27 19:28:01 -0600

    Dispatch the removeBook reducer

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,18 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
 export default function Book({
-  title, author,
+  id, title, author,
 }) {
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>{title}</h2>
       <p>{author}</p>
-      <button type="button">Remove</button>
+      <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
     </div>
   );
 }
 
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
