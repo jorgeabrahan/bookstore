79cb3655720378d0cf770192b27d21f31dded471
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-01 01:25:18 -0600

    Implement a disable prop on the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ run, text }) => {
+const Button = ({ run, text, disabled }) => {
   const onClick = (e) => {
     e.preventDefault();
     run();
   };
   return (
-    <button type="button" onClick={onClick}>{text}</button>
+    <button type="button" onClick={onClick} disabled={disabled}>{text}</button>
   );
 };
 
@@ -15,8 +15,10 @@ export default Button;
 Button.propTypes = {
   run: PropTypes.func.isRequired,
   text: PropTypes.string,
+  disabled: PropTypes.bool,
 };
 
 Button.defaultProps = {
   text: 'button',
+  disabled: false,
 };
