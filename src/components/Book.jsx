0924e8182f3ff3cf14e53375708fdd49a6ca152c
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-01 01:24:38 -0600

    Revert "Implement a disable prop on the button component"
    This reverts commit 4ed5e9a49380fdaf023c1f17cd4597429264f9f0.

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,29 +1,17 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
 import Button from './Button';
 
 export default function Book({
   id, title, author,
 }) {
-  const [error, setError] = useState('');
   const dispatch = useDispatch();
-  const { removing } = useSelector((store) => store.books);
-  const remove = () => {
-    try {
-      dispatch(removeBook(id)).unwrap();
-    } catch (error) {
-      setError(error);
-    }
-  };
-
   return (
     <div>
       <h2>{title}</h2>
       <p>{author}</p>
-      <Button run={remove} text="Remove" disabled={removing} />
-      {error !== '' && <p>{error}</p>}
+      <Button run={() => dispatch(removeBook(id))} text="Remove" />
     </div>
   );
 }
