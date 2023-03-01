4c9ca4637fb0be038d5f62b7ae1a9734a3a6e7ed
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-01 10:17:52 -0600

    Add background and border to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,34 +4,36 @@ import './Navbar.css';
 function Navbar() {
   return (
     <>
-      <nav className="center navbar">
-        <div className="navbar__content">
-          <h1 className="navbar__title">Bookstore CMS</h1>
-          <ul className="navbar__list">
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? '--active' : '')}
-              >
-                Books
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/categories"
-                className={({ isActive }) => (isActive ? '--active' : '')}
-              >
-                Categories
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <button type="button" className="navbar__button">
-            <span className="material-symbols-outlined">
-              person
-            </span>
-          </button>
+      <nav className="navbar-container">
+        <div className="center navbar">
+          <div className="navbar__content">
+            <h1 className="navbar__title">Bookstore CMS</h1>
+            <ul className="navbar__list">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) => (isActive ? '--active' : '')}
+                >
+                  Books
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/categories"
+                  className={({ isActive }) => (isActive ? '--active' : '')}
+                >
+                  Categories
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <button type="button" className="navbar__button">
+              <span className="material-symbols-outlined">
+                person
+              </span>
+            </button>
+          </div>
         </div>
       </nav>
       <Outlet />
